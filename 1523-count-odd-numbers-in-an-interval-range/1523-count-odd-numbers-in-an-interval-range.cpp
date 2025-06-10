bc2c622927505d36d6b5c1dcf81ff0d70e47f052
sanuthreddy <142322467+sanuthreddy@users.noreply.github.com>
2025-06-10 09:56:30 +0530

    Update 1523-count-odd-numbers-in-an-interval-range.cpp

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -7,12 +7,12 @@ public:
         }
         return cnt;*/
         int r=high-low+1;
-        if(r%2==0){
+        if(r%2==0){//length even means have equal no of even and odd
             return r>>1;
         }else{
-            if(low%2!=0 && high%2!=0){
+            if(low%2!=0 && high%2!=0){//3 4 5 6 7
                 return r/2 +1;
-            }else{
+            }else{//2 3 4 5
                 return r>>1;
             }
         }
