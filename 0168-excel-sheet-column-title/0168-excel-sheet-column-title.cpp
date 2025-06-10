18d933a3ee4790089dba864c286454ea31e3cdb5
sanuthreddy <142322467+sanuthreddy@users.noreply.github.com>
2025-06-10 08:06:31 +0530

    Update 0168-excel-sheet-column-title.cpp

--- 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     string convertToTitle(int columnNumber) {
-        string result;
+        string result;//to store answer
          while(columnNumber>0){
-              columnNumber--;
+              columnNumber--;//to handle corner cases like when 26 multiples
               int remainder=columnNumber%26;
-              char ch=remainder+'A';
+              char ch=remainder+'A';//as decremented before here restored to correct 
               result+=ch;
               columnNumber/=26;
          }
-         reverse(result.begin(),result.end());
+         reverse(result.begin(),result.end());//as we get reverse orrder of answer 
          return result;
     }
 };
