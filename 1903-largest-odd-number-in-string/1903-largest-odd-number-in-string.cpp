026ee9c8ed1ef14909db33667a58a5887b078086
sanuthreddy <142322467+sanuthreddy@users.noreply.github.com>
2025-06-10 10:54:43 +0530

    Update 1903-largest-odd-number-in-string.cpp

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string largestOddNumber(string num) {
         int s=-1;
         for(int i=num.size()-1;i>=0;i--){
-            if(num[i]%2!=0){
+            if(num[i]%2!=0){//checking the first odd from back as to get the largest length as in same ordre requires
                 s=i;
                 break;
             }
